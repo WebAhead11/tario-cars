79151771e3f9ade9a3ff7baceef532fd64fe8c7c
--replace-all <mario.saliba98@gmail.com>
2021-10-04 11:20:13 +0300

    adds the data for the cars

---
+++ src/data.js
@@ -0,0 +1,69 @@
+export const colors = [
+  "white",
+  "black",
+  "blue",
+  "yellow",
+  "green",
+  "pink",
+  "red",
+  "silver",
+  "purple",
+  "gray",
+  "champain",
+]
+
+export const carCompanies = [
+  {
+    name: "BMW",
+    models: ["520", "320", "118", "720", "235"],
+  },
+  {
+    name: "AUDI",
+    models: ["A1", "A3", "A5", "A7", "Q1", "Q3", "Q5", "Q7", "ETRON"],
+  },
+  {
+    name: "SEAT",
+    models: ["LEON", "IBIZA", "ARONA", "ATECA"],
+  },
+  {
+    name: "SKODA",
+    models: ["OCTAVIA", "RAPID", "KAMIQ", "KODIAQ", "SUPERB"],
+  },
+  { name: "SUBARU", models: ["FORSTER", "XV", "OUTBACK", "BRZ"] },
+  {
+    name: "HONDA",
+    models: ["CIVIC 4D", "CIVIC 5D", "CIVIC TYPE-R", "CRV", "HRV", "JAZZ"],
+  },
+  {
+    name: "NISSAN",
+    models: ["X-TRAIL", "QASHQAI", "JUKE", "MICRA"],
+  },
+  {
+    name: "VOLVO",
+    models: ["V40", "XC60", "XC90", "V60", "S90"],
+  },
+  {
+    name: "MITSUBISHI",
+    models: ["OUTLANDER", "ECLIPSE", "ASX", "SPACE STAR"],
+  },
+  {
+    name: "DODGE",
+    models: ["RAM"],
+  },
+  {
+    name: "FORD",
+    models: ["EDGE", "FOCUS", "KUGA", "PUMA"],
+  },
+  {
+    name: "BUICK",
+    models: ["LA CROSS", ""],
+  },
+  {
+    name: "TOYOTA",
+    models: ["COROLLA", "GT86", "LAND CRUISER", "RAV4", "HI LANDER"],
+  },
+  {
+    name: "KIA",
+    models: ["PICANTO", "SPORTAGE", "SORENTO", "OPTIMA"],
+  },
+]
